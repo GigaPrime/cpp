77dd64bdc9f675275c970798900de4c9e30a8195
Denis <noname_mailuser@ukr.net>
2017-03-06 01:30:38 +0200

    Work plan with stages for Vector

--- Classes/Class_Vector/Vector.h
+++ Classes/Class_Vector/Vector.h
@@ -12,3 +12,21 @@ private:
 
 #endif // !__VECTOR_H__
 
+/*
+	1. Define internal data structure (array), capacity, size, constant DEFAULT_CAPACITY;
+	2. Constructors & Destructor:
+		- default constructor (default vector capacity, defined with constants);
+		- constructor with param (capacity);
+		- copy constructor;
+		- destructor;
+	3. Methods 1:
+		- autoextend (private);
+		- add (at the end);
+		- get, [];
+		- set (replace element at particular index);
+	4. Methods 2:
+		- compress (capcity to size);
+		- autocompress (private);
+		- remove;
+		- subvector, 2 overloading (from/to exact, from the very beginning to exact index);
+*/
